6ab5c6aceb1dcb33b20f54b3135c80c73a2dc015
mastero101 <mastershift_101@hotmail.com>
2022-03-04 18:22:34 -0600

    ngif
    Ngif

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,9 +8,11 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     deshabilitar = false;
     mensaje = 'No se ha agregado ninguna persona'
-    titulo = '';
+    titulo = 'Ingeniero';
+    mostrar = false;
 
     agregarPersona(){
+        this.mostrar = true;
         this.mensaje = 'Persona agregada'
     }
 
